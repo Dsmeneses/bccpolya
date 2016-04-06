da819a2c223dfaeec0ace82f3d4f2209ac1821e2
Davi <davismeneses@hotmail.com>
2016-04-06 16:56:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,22 +6,21 @@ int main(){
 
     double numero;
     
-    printf("Entre com um número positivo : ");
+    printf("Entre com um número positivo: ");
     scanf("%lf",&numero);
     
-    printf("A raiz quadrada de %.3f é %.5f \n",numero ,raiz_quadrada(numero) );
+    printf("A raiz quadrada de %.3f é %.5f \n", numero, raiz_quadrada(numero));
 
     return 0;
 }
-
 
 double raiz_quadrada (double numero){
 
     int n;
     double a = numero;
     
-    for (n = 0; n < 10; ++n)
+    for (n = 0; n < 10; ++n){
           a = a/2 + numero/(2*a);
-            
-    return(a);    
+    }        
+    return(a);
 }   
