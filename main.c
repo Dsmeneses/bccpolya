75a40b3d42c5c3f8ecbf7be818cf7d7e321d7b13
Davi <davismeneses@hotmail.com>
2016-04-05 22:47:49 -0300

    Codigo Teste
    Codigo Teste para calcular a raiz

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double raiz_quadrada(double numero);
+
+int main(){ 
+
+    double numero;
+    
+    printf("Entre com um número positivo : ");
+    scanf("%lf",&numero);
+    
+    printf("A raiz quadrada de %.3f é %.5f \n",numero ,raiz_quadrada(numero) );
+
+    return 0;
+}
+
+
+double raiz_quadrada (double numero){
+
+    int n;
+    double recorre = numero;
+    
+    for (n = 0; n < 10; ++n)
+          recorre = recorre/2 + numero/(2*recorre);
+            
+    return(recorre);    
+}   
